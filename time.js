f774dcc92807739adcf593297db96761a19a88b0
Dhruv <dhruv20345@gmail.com>
2022-01-15 17:40:34 +0530

    added date

--- time.js
+++ time.js
@@ -1,3 +1,4 @@
+// time
 function timeHour() {
     var Ampm = "AM";
     let time = new Date();
@@ -14,3 +15,15 @@ function timeHour() {
     document.getElementById('clk').innerHTML = timeh + ":" + timem + ":" + times + " " + Ampm;
 }
 setInterval(timeHour, 1000);
+
+// Date
+const month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+let newDate = new Date();
+// console.log(newDate);
+let dy=newDate.getDate();
+let m=newDate.getMonth();
+if (dy < 10){
+    dy="0"+dy;
+}
+let yyyy=newDate.getFullYear();
+document.getElementById("day").innerHTML = dy+" "+month[m]+","+yyyy;
